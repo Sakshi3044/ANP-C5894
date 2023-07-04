6fd75b854f6dff4e66e801a98068e3265a47c5a6
Sakshi3044 <137878664+Sakshi3044@users.noreply.github.com>
2023-07-04 21:10:36 +0530

    Add files via upload

---
+++ Practicles/Hello_World.java
@@ -0,0 +1,13 @@
+//Program to print simple "hello world"
+
+package oops;
+
+public class Hello_World {
+
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World");
+
+	}
+
+}

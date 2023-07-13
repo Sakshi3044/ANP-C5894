210939278b7c1a75a7aca903fa715f546e0cdc13
Sakshi Shinde <137878664+Sakshi3044@users.noreply.github.com>
2023-07-13 11:12:14 +0530

    Add files via upload

---
+++ Practicles/PrintNumbers.java
@@ -0,0 +1,21 @@
+package loops;
+
+//print numbers from 1 to 10
+
+public class PrintNumbers
+
+{
+	public static void main(String[] args) 
+	
+	{
+		//num initialized to 1
+      int num = 1;
+      
+      //condition to check num is less than equal to 10
+      while (num <= 10)
+      {
+          System.out.println("Number is:"+num);
+          num++;
+      }
+  }
+}

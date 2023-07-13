898498d0e82cd037accbceda44151a6c066e8f12
Sakshi Shinde <137878664+Sakshi3044@users.noreply.github.com>
2023-07-13 11:08:58 +0530

    Add files via upload

---
+++ Practicles/ForExample.java
@@ -0,0 +1,17 @@
+package loops;
+
+//Simple For Loop example
+
+public class ForExample 
+{
+
+	public static void main(String[] args)
+	{
+		
+		for(int i=0;i<10;i++)
+		{
+			System.out.println("Values of i is :"+ i);
+		}
+
+	}
+}
